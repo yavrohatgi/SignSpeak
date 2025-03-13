48bf8a6a5291482f7bb66b92ee1efe81dea746d0
yavrohatgi <yavrohatgi03@gmail.com>
2025-03-13 15:40:20 -0400

    working version for cdr

--- newML/v2/inference_with_audio.py
+++ newML/v2/inference_with_audio.py
@@ -3,12 +3,94 @@ import struct
 import mmap
 import Adafruit_BBIO.PWM as PWM
 import time
+import board
+import adafruit_tca9548a
+import bmi270
+from bmi270.BMI270 import *
+import Adafruit_BBIO
+from micropython_bmi270 import bmi270
 
 import numpy as np
-import tensorflow.lite as tflite # This is for laptops/desktops
-# import tflite_runtime.interpreter as tflite # This is for BeagleBoneBlack
+#import tensorflow.lite as tflite # This is for laptops/desktops
+import tflite_runtime.interpreter as tflite # This is for BeagleBoneBlack
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module="tensorflow")
+
+
+class I2CWrapper:
+    def __init__(self, i2c_obj, address):
+        self._i2c = i2c_obj
+        self._address = address
+
+    def readfrom_mem(self, addr, reg, length, *_):
+        """Reads data from the specified register."""
+        self._i2c.writeto(self._address, bytes([reg]))  # Set register
+        result = bytearray(length)
+        self._i2c.readfrom_into(self._address, result)
+        return result
+
+    def writeto_mem(self, addr, reg, data, *_):
+        """Writes data to the specified register."""
+        self._i2c.writeto(self._address, bytes([reg]) + bytes(data))
+
+# Constants
+address_bmi270 = 0x68  # I2C address of the BMI270 sensor
+address_multiplexer = 0x68
+
+# Initialize I2C bus
+i2c = board.I2C()  # Uses board.SCL and board.SDA
+
+# Create the TCA9548A multiplexer object
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+# list of all channels of multiplexer in use (integers)
+used_channels = []
+
+# testing to make sure we recognize all connected IMUs on the proper channels
+for channel in range(8):
+    if tca[channel].try_lock():
+        print("Channel {}:".format(channel), end="")
+        addresses = tca[channel].scan()
+        # print([hex(address) for address in addresses if address != 0x70])
+        print([hex(address) for address in addresses])
+
+        if len(addresses) > 1:
+            used_channels.append(channel)
+        tca[channel].unlock()
+
+def collect_reading(used_channels):
+    """
+    returns a list of one gesture's readings over 1s
+    """
+    time.sleep(2)
+    print("Start gesture:")
+    print("In 2s")
+    time.sleep(1)
+    print("In 1s")
+    time.sleep(1)
+    print("Now:")
+    gesture_data_list = []
+    gesture_length_seconds = 1
+    gesture_datapoints = 10
+    num_fingers = 5
+    datapoints_per_reading = 6 # acc x, y, z, gyr x, y, z
+    max_readings = num_fingers * datapoints_per_reading * gesture_datapoints
+    curr_reading = 0
+    while curr_reading < max_readings:
+        for channel in used_channels:
+            if tca[channel].try_lock():
+                wrapped_i2c = I2CWrapper(tca[channel], address_bmi270)
+                sensor = bmi270.BMI270(wrapped_i2c)
+                for datapt in sensor.acceleration:
+                    gesture_data_list.append(datapt)
+                for datapt in sensor.gyro:
+                    gesture_data_list.append(datapt)
+                curr_reading += datapoints_per_reading
+            tca[channel].unlock()
+        time.sleep(gesture_length_seconds/gesture_datapoints)
+    print(gesture_data_list)
+    print(len(gesture_data_list))
+    return gesture_data_list
 
 def load_wav_as_array(filename):
     """Load a WAV file and return an array of samples."""
@@ -95,19 +177,21 @@ def main():
     input_details = interpreter.get_input_details()                              # Get input details
     output_details = interpreter.get_output_details()                            # Get output details
 
-    np.random.seed(0)                                                            # Set seed for reproducibility
-    x0 = np.random.rand(1, 300).astype(np.float32)                               # Generate a random input
+    # to use, just put in the list of the channels used on multiplexer (0, 1, 2, 3, 7)
+    # it returns a 300 point list of data points
+    reading1 = collect_reading(used_channels)                                    # Collect data
+    x0 = np.array(reading1, dtype=np.float32).reshape(1, -1) 
 
     # Run inference
-    interpreter.set_tensor(input_details[0]['index'], x0)                        # Load input
+    interpreter.set_tensor(input_details[0]['index'], x0)                  # Load input
     interpreter.invoke()                                                         # Run model
     predictions = interpreter.get_tensor(output_details[0]['index'])             # Get output
 
     predicted_class = np.argmax(predictions)                                     # Get majority class
     predicted_gesture = label_mapping[predicted_class]                           # Convert to original gesture
+    predicted_gesture = label_mapping.get(predicted_class, "Unknown") 
     print("Predicted Gesture:", predicted_gesture)                               # Print predicted gesture            
-
 
     play_audio(f"{predicted_gesture}.wav")                                       # Play the audio
 
-
+main()
